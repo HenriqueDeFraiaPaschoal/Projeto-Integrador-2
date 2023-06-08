35878465055a8941cdd551b0422e6473cfcf806a
denysfmalta <denysfmalta@hotmail.com>
2023-06-07 21:02:01 -0300

    add navigation w/o auth

--- FrontEnd/projeto-pi/src/components/PokeForm/PokeForm.js
+++ FrontEnd/projeto-pi/src/components/PokeForm/PokeForm.js
@@ -2,14 +2,19 @@ import React from 'react';
 import * as S from "./style.js";
 import Input from '../Input/Input.js';
 import Logo from '../Logo/Logo.js';
+import { useNavigate } from "react-router-dom";
 
 export const PokeIn = ({mainBtnText, redirectBtnText}) => {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate("/pokedex");
+  }
   return (
     <S.PokeIn>
       <Logo />
       <Input placeholder="T R E I N A D O R" type="email" value=""/>
       <Input placeholder="S E N H A" type="password" value=""/>
-      <S.btnLog>{mainBtnText}</S.btnLog>
+      <S.btnLog onClick={handleClick}>{mainBtnText}</S.btnLog>
       <S.btnRedirect>{redirectBtnText}</S.btnRedirect>
     </S.PokeIn>
   )
